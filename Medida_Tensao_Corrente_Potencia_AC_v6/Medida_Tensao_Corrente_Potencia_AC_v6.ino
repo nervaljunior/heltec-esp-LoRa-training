851c00d816f4f1cda789aca96227a0af7376ec49
Luan <barkon32@tuta.io>
2023-05-20 12:12:58 -0300

    adicionado print de teste e confirmado funcionamento

--- Medida_Tensao_Corrente_Potencia_AC_v6/Medida_Tensao_Corrente_Potencia_AC_v6.ino
+++ Medida_Tensao_Corrente_Potencia_AC_v6/Medida_Tensao_Corrente_Potencia_AC_v6.ino
@@ -42,6 +42,181 @@ String statusRele = "OFF"; // variavel para ler dados recebidos pela serial
 String off = "OFF";
 
 
+void setup()
+{
+  Serial.begin(115200); //Iniciando a comunicação serial
+  while (!Serial);
+  // Iniciando comunicação com o módulo LoRa
+  if (!LoRa.begin(LORA_BAND, BAND_915MHZ)) {
+  Serial.println("Erro ao iniciar o módulo LoRa");
+  while (true);
+}
+
+}
+void loop() {
+  Serial.print("Mensagem de teste\n");
+  #if MODO == 0
+  //Envio direto dos códigos do ADC sem nenhum tratamento
+
+  //Variáveis
+  int32_t tensao = 0;
+  int32_t corrente = 0;
+
+  //Captura
+  tensao = analogRead(pinAdcTensao);
+  corrente = analogRead(pinAdcCorrente);
+
+  //Transmissão
+  LoRa.beginPacket(); //Inicia o pacote de transmissão
+  LoRa.print(tensao); //Adiciona os dados ao pacote
+  LoRa.print(separador);
+  LoRa.print(corrente);
+  LoRa.endPacket(); //Encerra o pacote e envia os dados
+
+  delay(1000);
+  #endif
+
+  #if MODO == 1
+  //Envio dos códigos convertidos em tensão e corrente instatâneos
+
+  //coeficientes das regressões lineares. Usados para a conversão dos códigos
+  const float coefA_VAC = 0.2546;
+  const float coefB_VAC = 0.3807;
+  const float coefA_IAC = 0.117;
+  const float coefB_IAC = 0.1299;
+
+  //Variáveis
+  int32_t tensao = 0;
+  int32_t corrente = 0;
+
+  //Captura
+  tensao = analogRead(pinAdcTensao);
+  corrente = analogRead(pinAdcCorrente);
+
+  //Transmissão
+  //Serial.print(micros());
+  //Serial.print(separador);
+  LoRa.beginPacket();
+  LoRa.print((float(tensao) - offsetVAC)*coefA_VAC + coefB_VAC);
+  LoRa.print(separador);
+  LoRa.print((float(corrente) - offsetIAC)*coefA_IAC + coefB_IAC);
+  LoRa.endPacket(); //Encerra o pacote e envia os dados
+  delay(1000);
+
+  #endif
+
+
+  #if MODO == 2
+    //Envio dos códigos convertidos da tensão e corrente RMS
+
+    //coeficientes das regressões lineares. Usados para a conversão dos códigos
+    const float coefA_VAC = 0.2652;
+    const float coefB_VAC = 0.0;
+    const float coefA_IAC = 0.1191;
+    const float coefB_IAC = 0.0;
+
+    //Período mínimo de captura de 16,7ms para garantir que ao menos um ciclo seja capturado
+    const int32_t duracaoDaAmostragem = (int32_t)((1000.0 / 60.0) * 6.0); //6 ciclos de 1000ms/60 (60Hz)
+
+    //Variáveis
+    int32_t tensao = 0;
+    int32_t corrente = 0;
+    uint32_t qtdDeAmostras = 0;
+    float acumuladorTensao = 0.0;
+    float acumuladorCorrente = 0.0;
+
+    //Captura
+    unsigned long inicio = millis();
+    do
+    {
+      tensao = analogRead(pinAdcTensao) - offsetVAC;
+      corrente = analogRead(pinAdcCorrente) - offsetIAC;
+
+      acumuladorTensao = acumuladorTensao + (tensao * tensao); //Soma dos quadrados
+      acumuladorCorrente = acumuladorCorrente + (corrente * corrente);//Soma dos quadrados
+
+      qtdDeAmostras++;
+    } while ((millis() - inicio) < duracaoDaAmostragem);
+
+    //Cálculo dos valores RMS
+    acumuladorTensao = acumuladorTensao / float(qtdDeAmostras); //Média dos quadrados
+    acumuladorCorrente = acumuladorCorrente / float(qtdDeAmostras); //Média dos quadrados
+
+    acumuladorTensao = sqrt(acumuladorTensao); //RMS da captura
+    acumuladorCorrente = sqrt(acumuladorCorrente); //RMS da captura
+
+    acumuladorTensao = acumuladorTensao * coefA_VAC + coefB_VAC; //Conversão em volts
+    acumuladorCorrente = acumuladorCorrente * coefA_IAC + coefB_IAC; //Conversão em ampères
+
+    //Transmissão
+    String mensagem = String(acumuladorTensao) + separador + String(acumuladorCorrente);
+    LoRa.beginPacket();
+    LoRa.print(mensagem);
+    LoRa.endPacket();
+    delay(1000);
+  #endif
+
+
+  #if MODO == 3
+    //Envio dos códigos convertidos da tensão e corrente RMS (para o FK Serial)
+
+    //coeficientes das regressões lineares. Usados para a conversão dos códigos
+    const float coefA_VAC = 0.2652;
+    const float coefB_VAC = 0.0;
+    const float coefA_IAC = 0.1191;
+    const float coefB_IAC = 0.0;
+
+    //Período mínimo de captura de 16,7ms para garantir que ao menos um ciclo seja capturado
+    const int32_t duracaoDaAmostragem = (int32_t)((1000.0 / 60.0) * 6.0); //6 ciclos de 1000ms/60 (60Hz)
+
+    //Variáveis
+    int32_t tensao = 0;
+    int32_t corrente = 0;
+    uint32_t qtdDeAmostras = 0;
+    float acumuladorTensao = 0.0;
+    float acumuladorCorrente = 0.0;
+
+    //Captura
+    unsigned long inicio = millis();
+    do
+    {
+      tensao = analogRead(pinAdcTensao) - offsetVAC;
+      corrente = analogRead(pinAdcCorrente) - offsetIAC;
+
+      acumuladorTensao = acumuladorTensao + (tensao * tensao); //Soma dos quadrados
+      acumuladorCorrente = acumuladorCorrente + (corrente * corrente);//Soma dos quadrados
+
+      qtdDeAmostras++;
+    } while ((millis() - inicio) < duracaoDaAmostragem);
+
+    //Cálculo dos valores RMS
+    acumuladorTensao = acumuladorTensao / float(qtdDeAmostras); //Média dos quadrados
+    acumuladorCorrente = acumuladorCorrente / float(qtdDeAmostras); //Média dos quadrados
+
+    acumuladorTensao = sqrt(acumuladorTensao); //RMS da captura
+    acumuladorCorrente = sqrt(acumuladorCorrente); //RMS da captura
+
+    acumuladorTensao = acumuladorTensao * coefA_VAC + coefB_VAC; //Conversão em volts
+    acumuladorCorrente = acumuladorCorrente * coefA_IAC + coefB_IAC; //Conversão em ampères
+
+    //Transmissão
+    LoRa.beginPacket();
+    LoRa.print("t");
+    LoRa.print(separador);
+    LoRa.print(micros());
+    LoRa.print(separador);
+    LoRa.print("Vac");
+    LoRa.print(separador);
+    LoRa.print(acumuladorTensao);
+    LoRa.print(separador);
+    LoRa.print("Iac");
+    LoRa.print(separador);
+    LoRa.println(acumuladorCorrente);
+    LoRa.endPacket();
+    delay(1000);
+  #endif
+}
+
 
  void ativarRele()
 {
@@ -62,179 +237,4 @@ String off = "OFF";
     Serial.println(String(readStatus));
     digitalWrite(RelePin, LOW);
   }
-}
-
-void setup()
-{
-  Serial.begin(115200); //Iniciando a comunicação serial
-  while (!Serial);
-  // Iniciando comunicação com o módulo LoRa
-  if (!LoRa.begin(LORA_BAND, BAND_915MHZ)) {
-  Serial.println("Erro ao iniciar o módulo LoRa");
-  while (true);
-}
-
-}
-void loop() {
-
-#if MODO == 0
-//Envio direto dos códigos do ADC sem nenhum tratamento
-
-//Variáveis
-int32_t tensao = 0;
-int32_t corrente = 0;
-
-//Captura
-tensao = analogRead(pinAdcTensao);
-corrente = analogRead(pinAdcCorrente);
-
-//Transmissão
-LoRa.beginPacket(); //Inicia o pacote de transmissão
-LoRa.print(tensao); //Adiciona os dados ao pacote
-LoRa.print(separador);
-LoRa.print(corrente);
-LoRa.endPacket(); //Encerra o pacote e envia os dados
-
-delay(1000);
-#endif
-
-#if MODO == 1
-//Envio dos códigos convertidos em tensão e corrente instatâneos
-
-//coeficientes das regressões lineares. Usados para a conversão dos códigos
-const float coefA_VAC = 0.2546;
-const float coefB_VAC = 0.3807;
-const float coefA_IAC = 0.117;
-const float coefB_IAC = 0.1299;
-
-//Variáveis
-int32_t tensao = 0;
-int32_t corrente = 0;
-
-//Captura
-tensao = analogRead(pinAdcTensao);
-corrente = analogRead(pinAdcCorrente);
-
-//Transmissão
-//Serial.print(micros());
-//Serial.print(separador);
-LoRa.beginPacket();
-LoRa.print((float(tensao) - offsetVAC)*coefA_VAC + coefB_VAC);
-LoRa.print(separador);
-LoRa.print((float(corrente) - offsetIAC)*coefA_IAC + coefB_IAC);
-LoRa.endPacket(); //Encerra o pacote e envia os dados
-delay(1000);
-
-#endif
-
-
-#if MODO == 2
-  //Envio dos códigos convertidos da tensão e corrente RMS
-
-  //coeficientes das regressões lineares. Usados para a conversão dos códigos
-  const float coefA_VAC = 0.2652;
-  const float coefB_VAC = 0.0;
-  const float coefA_IAC = 0.1191;
-  const float coefB_IAC = 0.0;
-
-  //Período mínimo de captura de 16,7ms para garantir que ao menos um ciclo seja capturado
-  const int32_t duracaoDaAmostragem = (int32_t)((1000.0 / 60.0) * 6.0); //6 ciclos de 1000ms/60 (60Hz)
-
-  //Variáveis
-  int32_t tensao = 0;
-  int32_t corrente = 0;
-  uint32_t qtdDeAmostras = 0;
-  float acumuladorTensao = 0.0;
-  float acumuladorCorrente = 0.0;
-
-  //Captura
-  unsigned long inicio = millis();
-  do
-  {
-    tensao = analogRead(pinAdcTensao) - offsetVAC;
-    corrente = analogRead(pinAdcCorrente) - offsetIAC;
-
-    acumuladorTensao = acumuladorTensao + (tensao * tensao); //Soma dos quadrados
-    acumuladorCorrente = acumuladorCorrente + (corrente * corrente);//Soma dos quadrados
-
-    qtdDeAmostras++;
-  } while ((millis() - inicio) < duracaoDaAmostragem);
-
-  //Cálculo dos valores RMS
-  acumuladorTensao = acumuladorTensao / float(qtdDeAmostras); //Média dos quadrados
-  acumuladorCorrente = acumuladorCorrente / float(qtdDeAmostras); //Média dos quadrados
-
-  acumuladorTensao = sqrt(acumuladorTensao); //RMS da captura
-  acumuladorCorrente = sqrt(acumuladorCorrente); //RMS da captura
-
-  acumuladorTensao = acumuladorTensao * coefA_VAC + coefB_VAC; //Conversão em volts
-  acumuladorCorrente = acumuladorCorrente * coefA_IAC + coefB_IAC; //Conversão em ampères
-
-  //Transmissão
-  String mensagem = String(acumuladorTensao) + separador + String(acumuladorCorrente);
-  LoRa.beginPacket();
-  LoRa.print(mensagem);
-  LoRa.endPacket();
-  delay(1000);
-#endif
-
-
-#if MODO == 3
-  //Envio dos códigos convertidos da tensão e corrente RMS (para o FK Serial)
-
-  //coeficientes das regressões lineares. Usados para a conversão dos códigos
-  const float coefA_VAC = 0.2652;
-  const float coefB_VAC = 0.0;
-  const float coefA_IAC = 0.1191;
-  const float coefB_IAC = 0.0;
-
-  //Período mínimo de captura de 16,7ms para garantir que ao menos um ciclo seja capturado
-  const int32_t duracaoDaAmostragem = (int32_t)((1000.0 / 60.0) * 6.0); //6 ciclos de 1000ms/60 (60Hz)
-
-  //Variáveis
-  int32_t tensao = 0;
-  int32_t corrente = 0;
-  uint32_t qtdDeAmostras = 0;
-  float acumuladorTensao = 0.0;
-  float acumuladorCorrente = 0.0;
-
-  //Captura
-  unsigned long inicio = millis();
-  do
-  {
-    tensao = analogRead(pinAdcTensao) - offsetVAC;
-    corrente = analogRead(pinAdcCorrente) - offsetIAC;
-
-    acumuladorTensao = acumuladorTensao + (tensao * tensao); //Soma dos quadrados
-    acumuladorCorrente = acumuladorCorrente + (corrente * corrente);//Soma dos quadrados
-
-    qtdDeAmostras++;
-  } while ((millis() - inicio) < duracaoDaAmostragem);
-
-  //Cálculo dos valores RMS
-  acumuladorTensao = acumuladorTensao / float(qtdDeAmostras); //Média dos quadrados
-  acumuladorCorrente = acumuladorCorrente / float(qtdDeAmostras); //Média dos quadrados
-
-  acumuladorTensao = sqrt(acumuladorTensao); //RMS da captura
-  acumuladorCorrente = sqrt(acumuladorCorrente); //RMS da captura
-
-  acumuladorTensao = acumuladorTensao * coefA_VAC + coefB_VAC; //Conversão em volts
-  acumuladorCorrente = acumuladorCorrente * coefA_IAC + coefB_IAC; //Conversão em ampères
-
-  //Transmissão
-  LoRa.beginPacket();
-  LoRa.print("t");
-  LoRa.print(separador);
-  LoRa.print(micros());
-  LoRa.print(separador);
-  LoRa.print("Vac");
-  LoRa.print(separador);
-  LoRa.print(acumuladorTensao);
-  LoRa.print(separador);
-  LoRa.print("Iac");
-  LoRa.print(separador);
-  LoRa.println(acumuladorCorrente);
-  LoRa.endPacket();
-  delay(1000);
-#endif
 }
